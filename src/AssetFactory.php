e2358e08c214921771a1f2e1c20ad88534e29801
Daniel Mejta <daniel@mejta.net>
2022-03-29 14:11:44 +0000

    Update AssetFactory.php

--- src/AssetFactory.php
+++ src/AssetFactory.php
@@ -15,19 +15,38 @@ class AssetFactory {
 		$config      = new AssetConfig( $args );
 		$asset_base  = pathinfo( $asset_path, PATHINFO_FILENAME );
 		$build_path  = pathinfo( $asset_path, PATHINFO_DIRNAME );
-		$asset_info  = wp_normalize_path( $build_path . '/' . $asset_base . '.asset.php' );
 		$assets_info = wp_normalize_path( $build_path . '/assets.php' );
 		$extension   = current( explode( '?', pathinfo( $asset_path, PATHINFO_EXTENSION ) ) );
 		$filename    = $asset_base . '.' . $extension;
+		$asset_info  = array( wp_normalize_path( $build_path . '/' . $asset_base . '.asset.php' ) );
+
+		if ( AssetConfigInterface::TYPE_STYLE ) {
+			$asset_info[] = preg_replace( '/style-(\S+?)\.css$/', '$1.asset.php', $asset_path );
+		}
 
 		if ( file_exists( $asset_path ) ) {
 			$config->set_src( str_replace( WP_CONTENT_DIR, content_url(), $asset_path ) );
 		}
 
-		if ( file_exists( $asset_info ) ) {
-			$info = require $asset_info;
+		$info = null;
 
-			$config->set_version( $info['version'] ?? $config->get_version() );
+		if ( file_exists( $assets_info ) ) {
+			$infos = require $assets_info;
+
+			if ( isset( $infos[ $asset_path ] ) ) {
+				$info = $infos[ $asset_path ];
+			}
+		} else {
+			foreach ( $asset_info as $asset_info_item ) {
+				if ( file_exists( $asset_info_item ) ) {
+					$info = require $asset_info_item;
+					break;
+				}
+			}
+		}
+
+		if ( ! empty( $info ) ) {
+			$config->set_version( $info['version'] ?? $config->get_version() ?? '' );
 
 			if ( $config->get_type() === AssetConfigInterface::TYPE_SCRIPT ) {
 				$config->merge_dependencies( $info['dependencies'] ?? array() );
